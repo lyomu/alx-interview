f2848c4ab9f67783a0893976b03d42fc5e9c6fb3
Gideon Lyomu <65831672+lyomu@users.noreply.github.com>
2023-05-03 00:45:13 +0300

    Fixes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-"""
-You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1
-and each box may contain keys to the other boxes
-More info:
-    - Prototype: def canUnlockAll(boxes)
-    - boxes is a list of lists
-    - A key with the same number as a box opens that box
-    - You can assume all keys will be positive integers
-    - The first box boxes[0] is unlocked
-    - Return True if all boxes can be opened, else return False
-    """
+'''A module for working with lockboxes.
+'''
 
 
-    def canUnlockAll(boxes):
-            """
-                Description:
-                    Write a method that determines if all the boxes can be opened
-                        Arguments:
-                            boxes --> List of Lists, it contains the boxes with keys
-                                Reurn boolean
-                                    Variables:
-                                        myKeys --> List, Store the number keys to open boxes
-                                            key --> integer, key of the myKeys
-                                                boxKey --> iinteger, key inside of an specific box
-                                                    """
-                                                        myKeys = [0]
-                                                            for key in myKeys:
-                                                                        for boxKey in boxes[key]:
-                                                                                        if boxKey not in myKeys and boxKey < len(boxes):
-                                                                                                            myKeys.append(boxKey)
-                                                                                                                if len(myKeys) == len(boxes):
-                                                                                                                            return True
-                                                                                                                            return False
+def canUnlockAll(boxes):
+        '''Checks if all the boxes in a list of boxes containing the keys
+            (indices) to other boxes can be unlocked given that the first
+                box is unlocked.
+                    '''
+                        n = len(boxes)
+                            seen_boxes = set([0])
+                                unseen_boxes = set(boxes[0]).difference(set([0]))
+                                    while len(unseen_boxes) > 0:
+                                                boxIdx = unseen_boxes.pop()
+                                                        if not boxIdx or boxIdx >= n or boxIdx < 0:
+                                                                        continue
+                                                                            if boxIdx not in seen_boxes:
+                                                                                            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+                                                                                                        seen_boxes.add(boxIdx)
+                                                                                                            return n == len(seen_boxes)
